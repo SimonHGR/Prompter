a22fe42516b66f8d467b2c4e445113200bc8a0ef
Simon Roberts <simon@dancingcloudservices.com>
2014-04-13 12:45:55 -0600

    Added command line parameter for controlling scroll increment so that smooth scrolling isn't the same thing as "incredibly slow" scrolling

--- prompter/Prompter.java
+++ prompter/Prompter.java
@@ -20,9 +20,15 @@ import javax.swing.SwingUtilities;
 public class Prompter extends JFrame {
 
     private static class PrompterText extends JTextArea {
+        private int scrollIncrement = 2;
+        
+        public PrompterText(int scrollIncrement) {
+            this.scrollIncrement = scrollIncrement;
+        }
+        
         @Override
         public int getScrollableUnitIncrement(Rectangle r, int orient, int direction) {
-            return 2;
+            return scrollIncrement;
         }
         
         @Override
@@ -37,19 +43,15 @@ public class Prompter extends JFrame {
         }
     }
 
-    private JTextArea jta = new PrompterText();
-    private JScrollPane jscroll = new JScrollPane(jta);
+    private JTextArea jta;
+    private JScrollPane jscroll;
 
-    public Prompter(String text, int fontSize) {
+    public Prompter(String text, int fontSize, int scrollIncrement) {
         setAlwaysOnTop(true);
         setUndecorated(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jscroll.setHorizontalScrollBarPolicy(
-                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
-        jscroll.setVerticalScrollBarPolicy(
-                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
+        jta = new PrompterText(scrollIncrement);
         jta.setForeground(Color.WHITE);
         jta.setBackground(Color.BLACK);
         jta.setText(text);
@@ -58,7 +60,14 @@ public class Prompter extends JFrame {
         jta.setFont(new Font("Sans", Font.BOLD, fontSize));
         jta.setCaretPosition(0);
         jta.setAutoscrolls(true);
+        jscroll = new JScrollPane(jta);
         add(jscroll, BorderLayout.CENTER);
+        jscroll.setHorizontalScrollBarPolicy(
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        jscroll.setVerticalScrollBarPolicy(
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
         setBounds(0, 0, 1280, 720);
         setVisible(true);
 
@@ -77,7 +86,7 @@ public class Prompter extends JFrame {
 
     public static void main(String[] args) throws Throwable {
         if (args.length == 0) {
-            System.err.println("Usage: java -jar prompter.jar <file-to-display> [font-size]");
+            System.err.println("Usage: java -jar prompter.jar <file-to-display> [font-size] [scroll-increment]");
             System.exit(1);
         }
         
@@ -90,16 +99,21 @@ public class Prompter extends JFrame {
         final String text = new String(buffer, 0, (int) size);
 
         int fs = 64;
-        if (args.length == 2) {
+        if (args.length >= 2) {
             fs = Integer.parseInt(args[1]);
         }
         final int fontSize = fs;
         
+        int si = 4;
+        if (args.length >= 3) {
+            si = Integer.parseInt(args[2]);
+        }
+        final int scrollIncrement = si;
+        
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new Prompter(text, fontSize);
+                new Prompter(text, fontSize, scrollIncrement);
             }
         });
     }
-
 }
